b6601c975291713adfb825d524d766cdc60aa988
Matt Burke <burkemd1+github@gmail.com>
2014-10-10 20:27:45 -0400

    render the ast to css

---
+++ SassSharp.Tests/RendererTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SassSharp.Ast;
+
+namespace SassSharp.Tests
+{
+    [TestFixture]
+    public class RendererTest
+    {
+        public Node TestData
+        {
+            get
+            {
+                var rootNode = Node.Create("p",
+                    DeclarationSet.Create(
+                        new Declaration("color", "red"),
+                        new Declaration("font-weight", "bold")
+                    ),
+                    Node.Create("a",
+                        DeclarationSet.Create(
+                            new Declaration("font-size", "12px")
+                        )
+                    )
+                );
+
+                return rootNode;
+
+            }
+        }
+
+        [Test]
+        public void TestCompilesSimpleNodeStructureToCss()
+        {
+            var renderer = new Renderer();
+
+            var css = renderer.Render(TestData);
+
+            Assert.That(css, Is.EqualTo("p{color:red;font-weight:bold;}p a{font-size:12px;}"));
+        }
+    }
+}
---
+++ SassSharp/Renderer.cs
@@ -0,0 +1,41 @@
+﻿using SassSharp.Ast;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SassSharp
+{
+    public class Renderer
+    {
+        public Renderer()
+        {
+
+        }
+
+        public string Render(Node root)
+        {
+            StringBuilder sb = new StringBuilder();
+            Transpiler t = new Transpiler();
+
+            foreach(var rule in t.Transpile(root))
+            {
+                sb.Append(rule.Selector);
+                sb.Append('{');
+                
+                foreach(var declaration in rule.Declarations)
+                {
+                    sb.Append(declaration.Property);
+                    sb.Append(':');
+                    sb.Append(declaration.Value);
+                    sb.Append(';');
+                }
+                sb.Append('}');
+            }
+            return sb.ToString();
+        }
+
+
+    }
+}

bf8bbb67765745e6304a1ba8a68e48ef2b77bb83
Matt Burke <burkemd1+github@gmail.com>
2014-10-16 02:50:46 -0400

    rename Ast.SyntaxTreeBuilder to Parser

--- SassSharp/Parser.cs
+++ SassSharp/Parser.cs
@@ -4,8 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SassSharp.Ast;
 
-namespace SassSharp.Ast
+namespace SassSharp
 {
     public class SyntaxException : Exception
     {

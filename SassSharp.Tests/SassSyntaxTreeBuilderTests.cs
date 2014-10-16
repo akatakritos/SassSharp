f14ad74734fed799af340be2c5a7ca142c5f32cd
Matt Burke <burkemd1+github@gmail.com>
2014-10-16 02:38:50 -0400

    add SassSyntaxTreeBuilder

---
+++ SassSharp.Tests/SassSyntaxTreeBuilderTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using SassSharp.Ast;
+using SassSharp.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SassSharp.Tests
+{
+    [TestFixture]
+    public class SassSyntaxTreeBuilderTests
+    {
+        [Test]
+        public void TestBuildsSimpleTree()
+        {
+            var tokenStream = new Token[]
+            {
+                new Token(TokenType.Identifier, "p"),
+                new Token(TokenType.OpenBrace, "{"),
+                new Token(TokenType.Identifier, "color"),
+                new Token(TokenType.Colon, ":"),
+                new Token(TokenType.Identifier, "red"),
+                new Token(TokenType.SemiColon, ";"),
+                new Token(TokenType.CloseBrace, "}")
+            };
+
+            var builder = new SassSyntaxTreeBuilder();
+            var root = builder.Build(tokenStream).Root.Children.First();
+
+            Assert.That(root, Is.TypeOf<SassNode>());
+
+            var sassNode = (SassNode)root;
+            Assert.That(sassNode.Selector.Selector.Value, Is.EqualTo("p"));
+
+            var declaration = (DeclarationNode)sassNode.Container.Children.First();
+            Assert.That(declaration.Property.Name, Is.EqualTo("color"));
+            Assert.That(declaration.Value.Value, Is.EqualTo("red"));
+        }
+    }
+}
---
+++ SassSharp/Ast/SassSyntaxTreeBuilder.cs
@@ -0,0 +1,66 @@
+﻿using SassSharp.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SassSharp.Ast
+{
+    public class SassSyntaxTreeBuilder
+    {
+         private Stack<Token> tokenStack;
+        private Stack<NodeBuilder> nodeStack;
+
+        private NodeBuilder CurrentNode
+        {
+            get { return nodeStack.Peek(); }
+        }
+
+        public SassSyntaxTreeBuilder()
+        {
+            this.tokenStack = new Stack<Token>();
+            this.nodeStack = new Stack<NodeBuilder>();
+            this.nodeStack.Push(new NodeBuilder("")); //root node
+        }
+
+        public SassSyntaxTree2 Build(IEnumerable<Token> tokens)
+        {
+            foreach (var token in tokens)
+            {
+                //Console.WriteLine(token);
+                if (token.Type == TokenType.OpenBrace)
+                {
+                    var selector = tokenStack.Pop();
+
+                    continue;
+                }
+
+                if (token.Type == TokenType.SemiColon)
+                {
+                    var value = tokenStack.Pop().Value;
+                    tokenStack.Pop(); // colon
+                    var property = tokenStack.Pop().Value;
+
+                    CurrentNode.AddDeclaration(new Declaration(property, value));
+                    continue;
+                }
+
+                if (token.Type == TokenType.CloseBrace)
+                {
+                    var child = nodeStack.Pop();
+                    CurrentNode.AddChild(child.ToNode());
+                    continue;
+                }
+
+                tokenStack.Push(token);
+
+
+            }
+
+            var rootNode = CurrentNode.ToNode();
+            return new SassSyntaxTree2(rootNode);
+        }
+
+    }
+}

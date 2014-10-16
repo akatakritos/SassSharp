88c110a2556859fadc729f2e0e3e43edd9305e79
Matt Burke <burkemd1+github@gmail.com>
2014-10-16 02:37:33 -0400

    add SassSyntaxTree2

---
+++ SassSharp/Ast/SassSyntaxTree2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SassSharp.Ast
+{
+    public class SassSyntaxTree2
+    {
+        private IEnumerable<RuleNode> enumerable;
+
+        public RootNode Root { get; private set; }
+
+        public SassSyntaxTree2(RootNode root)
+        {
+            this.Root = root;
+        }
+
+        public SassSyntaxTree2(IEnumerable<RuleNode> enumerable)
+        {
+            // TODO: Complete member initialization
+            this.enumerable = enumerable;
+        }
+    }
+}

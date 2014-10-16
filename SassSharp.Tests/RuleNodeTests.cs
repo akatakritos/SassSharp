f199951ca82fc7f0783a4fe8e106671b0f66b6f4
Matt Burke <burkemd1+github@gmail.com>
2014-10-16 02:33:05 -0400

    TMP added empty test

---
+++ SassSharp.Tests/RuleNodeTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using SassSharp.Ast;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SassSharp.Tests
+{
+    [TestFixture]
+    public class RuleNodeTests
+    {
+    }
+}

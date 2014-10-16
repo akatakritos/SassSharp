54bb5ad7fa1f82e283bf1a0eeb8ffd86318087d9
Matt Burke <burkemd1+github@gmail.com>
2014-10-15 21:39:32 -0400

    remove old copied code

--- SassSharp/Ast/SassSyntaxTree2.cs
+++ SassSharp/Ast/SassSyntaxTree2.cs
@@ -8,19 +8,11 @@ namespace SassSharp.Ast
 {
     public class SassSyntaxTree2
     {
-        private IEnumerable<RuleNode> enumerable;
-
         public RootNode Root { get; private set; }
 
         public SassSyntaxTree2(RootNode root)
         {
             this.Root = root;
-        }
-
-        public SassSyntaxTree2(IEnumerable<RuleNode> enumerable)
-        {
-            // TODO: Complete member initialization
-            this.enumerable = enumerable;
         }
     }
 }

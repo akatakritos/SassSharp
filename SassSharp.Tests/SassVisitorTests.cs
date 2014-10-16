546649ec1e0d545e7db739dc094232c48f279d07
Matt Burke <burkemd1+github@gmail.com>
2014-10-16 02:35:58 -0400

    TMP add empty visitor test

---
+++ SassSharp.Tests/SassVisitorTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SassSharp.Tests
+{
+    [TestFixture]
+    public class SassVisitorTests
+    {
+        public void TestCallsSpecificOverride()
+        {
+            
+        }
+    }
+}

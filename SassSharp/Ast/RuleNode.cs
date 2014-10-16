4eb5dd05b843611f0463f838ca807457d02d5d17
Matt Burke <burkemd1+github@gmail.com>
2014-10-16 02:31:07 -0400

    Create clases for new AST model

--- SassSharp/Ast/RuleNode.cs
+++ SassSharp/Ast/RuleNode.cs
@@ -6,7 +6,97 @@ using System.Threading.Tasks;
 
 namespace SassSharp.Ast
 {
-    public class RuleNode
+    public interface INodeVisitor<R, P>
+    {
+        R Visit(RootNode node, P p);
+        R Visit(RuleNode node, P p);
+        R Visit(SelectorNode node, P p);
+        R Visit(SassNode node, P p);
+        R Visit(SassContainerNode node, P p);
+        R Visit(DeclarationNode node, P p);
+        R Visit(PropertyNode node, P p);
+        R Visit(ValueNode node, P p);
+    }
+
+    public abstract class Node
+    {
+        public abstract R Accept<R, P>(INodeVisitor<R,P> visitor, P p);
+    }
+
+    public class RootNode : Node
+    {
+        public IEnumerable<Node> Children { get; private set; }
+
+        public override R Accept<R, P>(INodeVisitor<R, P> visitor, P p)
+        {
+            return visitor.Visit(this, p);
+        }
+    }
+
+
+    public class SelectorNode : Node
+    {
+        public Selector Selector { get; private set; }
+
+        public override R Accept<R,P>(INodeVisitor<R,P> visitor, P p)
+        {
+            return visitor.Visit(this, p);
+        }
+    }
+
+    public class SassNode : Node
+    {
+        public SelectorNode Selector { get; private set; }
+        public SassContainerNode Container { get; private set; }
+        public override R Accept<R, P>(INodeVisitor<R, P> visitor, P p)
+        {
+            return visitor.Visit(this, p);
+        }
+    }
+
+    public class SassContainerNode : Node
+    {
+        public IEnumerable<Node> Children { get; private set; }
+
+        public override R Accept<R,P>(INodeVisitor<R,P> visitor, P p)
+        {
+            return visitor.Visit(this, p);
+        }
+    }
+
+    public class DeclarationNode : Node
+    {
+        public PropertyNode Property { get; private set; }
+        public ValueNode Value { get; private set; }
+
+        public override R Accept<R, P>(INodeVisitor<R, P> visitor, P p)
+        {
+            return visitor.Visit(this, p);
+        }
+    }
+
+    public class PropertyNode : Node
+    {
+        public string Name { get; private set; }
+
+        public override R Accept<R, P>(INodeVisitor<R, P> visitor, P p)
+        {
+            return visitor.Visit(this, p);
+        }
+    }
+
+    public class ValueNode : Node
+    {
+        public string Value { get; private set; }
+
+        public override R Accept<R, P>(INodeVisitor<R, P> visitor, P p)
+        {
+            return visitor.Visit(this, p);
+        }
+    }
+
+
+    public class RuleNode : Node
     {
         public Selector Selector { get; private set; }
         public DeclarationSet Declarations { get; private set; }
@@ -40,6 +130,11 @@ namespace SassSharp.Ast
                 Declarations = declarations,
                 Children = children
             };
+        }
+
+        public override R Accept<R, P>(INodeVisitor<R, P> visitor, P p)
+        {
+            return visitor.Visit(this, p);
         }
     }
 }

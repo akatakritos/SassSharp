529ded3ba653afa616999afd85054067f8d4dfb8
Matt Burke <burkemd1+github@gmail.com>
2014-10-22 09:23:41 -0500

    Use a custom constraint instead of private assertion function
    Might be more flexible

---
+++ SassSharp.Tests/Contraints/TokensMatchRegardlessOfPositionConstraint.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework.Constraints;
+using SassSharp.Tokens;
+
+namespace SassSharp.Tests.Contraints
+{
+    internal class TokensMatchRegardlessOfPositionConstraint : Constraint
+    {
+        private Token[] expected;
+        private TokenDifference difference;
+        private int differentIndex;
+
+        public TokensMatchRegardlessOfPositionConstraint(Token[] expected)
+        {
+            this.expected = expected;
+        }
+
+        public override bool Matches(object actual)
+        {
+            base.actual = actual;
+
+            return assertSameTokenStreamRegardlessOfPosition(expected, (Token[])actual);
+        }
+
+        public override void WriteDescriptionTo(MessageWriter writer)
+        {
+            writer.WriteExpectedValue(expected);
+        }
+
+        public override void WriteMessageTo(MessageWriter writer)
+        {
+            if (difference == TokenDifference.StreamLength)
+            {
+                writer.WriteLine("Token streams are different lengths: Expected {0}, Got {1}",
+                    expected.Length, ((Token[])actual).Length);
+            } else if (difference == TokenDifference.Type)
+            {
+                writer.WriteLine("Tokens differ by Type at index {0}: Expected '{1}', Got '{2}'",
+                    differentIndex, expected[differentIndex].Type, ((Token[])actual)[differentIndex].Type);
+            }
+            else if (difference == TokenDifference.Value)
+            {
+                writer.WriteLine("Tokens differ by Value at index {0}: Expected '{1}', Got '{2}'",
+                    differentIndex, expected[differentIndex].Value, ((Token[])actual)[differentIndex].Value);
+            }
+        }
+
+        private bool assertSameTokenStreamRegardlessOfPosition(Token[] expected, Token[] actual)
+        {
+            if (expected.Length != actual.Length)
+            {
+                difference = TokenDifference.StreamLength;
+                differentIndex = 0;
+                return false;
+            }
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                var ac = actual[i];
+                var ex = expected[i];
+
+                if (ex.Type != ac.Type)
+                {
+                    difference = TokenDifference.Type;
+                    differentIndex = i;
+                    return false;
+                }
+
+                if (ex.Value != ac.Value)
+                {
+                    difference = TokenDifference.Value;
+                    differentIndex = i;
+                    return false;
+                }
+
+            }
+
+            return true;
+        }
+
+        private enum TokenDifference
+        {
+            StreamLength,
+            Type,
+            Value
+        }
+    }
+}
--- SassSharp.Tests/TokenizerTests.cs
+++ SassSharp.Tests/TokenizerTests.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using SassSharp.Tokens;
+using SassSharp.Tests.Contraints;
 
 namespace SassSharp.Tests
 {
@@ -49,23 +50,7 @@ namespace SassSharp.Tests
             //    Console.WriteLine("{0}: {1}   => {2}: {3}", ex.Type, ex.Value, ac.Type, ac.Value);
             //}
 
-            assertSameTokenStreamRegardlessOfPosition(expected, tokens);
-        }
-
-        private void assertSameTokenStreamRegardlessOfPosition(Token[] expected, Token[] actual)
-        {
-            Assert.That(expected.Length, Is.EqualTo(actual.Length), string.Format("Token streams dont have the same length. Expected {0}, got {1}", expected.Length, actual.Length));
-
-            for (int i = 0; i < expected.Length; i++)
-            {
-                var ac = actual[i];
-                var ex = expected[i];
-
-                Assert.That(ex.Type, Is.EqualTo(ac.Type), string.Format(
-                    "Types dont match at index {0} : Expected {1}, got {2}", i, ex.Type, ac.Type));
-                Assert.That(ex.Value, Is.EqualTo(ac.Value), string.Format(
-                    "Values dont match at index {0}: Expected {1}, got {2}", i, ex.Value, ac.Value));
-            }
+            Assert.That(tokens, new TokensMatchRegardlessOfPositionConstraint(expected));
         }
 
         [Test]

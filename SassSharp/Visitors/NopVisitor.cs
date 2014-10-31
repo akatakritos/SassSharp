85d4aad0396551b57eb47069e5398eb5a182135b
Matt Burke <burkemd1+github@gmail.com>
2014-10-30 20:54:02 -0500

    NopVisitor - Visits nodes in the tree but does nothing (noop)
    Useful for subclasses that are only interested in a few node types: they dont have to implement all of the INodeVisitor interface

---
+++ SassSharp/Visitors/NopVisitor.cs
@@ -0,0 +1,52 @@
+﻿using SassSharp.Ast;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SassSharp.Visitors
+{
+    internal class NopVisitor<R,P> : INodeVisitor<R,P>
+    {
+        public virtual R Visit(RootNode node, P p)
+        {
+            return default(R);
+        }
+
+        public virtual R Visit(SelectorNode node, P p)
+        {
+            return default(R);
+        }
+
+        public virtual R Visit(SassNode node, P p)
+        {
+            return default(R);
+        }
+
+        public virtual R Visit(SassContainerNode node, P p)
+        {
+            return default(R);
+        }
+
+        public virtual R Visit(DeclarationNode node, P p)
+        {
+            return default(R);
+        }
+
+        public virtual R Visit(PropertyNode node, P p)
+        {
+            return default(R);
+        }
+
+        public virtual R Visit(ValueNode node, P p)
+        {
+            return default(R);
+        }
+
+        public virtual R Visit(AtCommandNode node, P p)
+        {
+            return default(R);
+        }
+    }
+}
